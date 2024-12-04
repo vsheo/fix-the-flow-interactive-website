58375b1761c21cbdcd67f7f3a0bdea9a45b979e6
vsheo <vireshsheoratan@gmail.com>
2024-12-04 11:55:37 +0100

    mobile first css #3
    alleen css voor de mobiele versie

---
+++ scripts/openNav.js
@@ -0,0 +1,14 @@
+// deze script selecteerd een button die de nav open maaktr.
+// de javascript heeft een eventlistener op de button.
+// Als het geklikt wordt dan wordt er een class toegevoed of weggehaald(toggle) aan de button.
+
+
+// selecteer de button
+let navButton = document.querySelector('.radio_menu')
+// selecteer de nav die op beeld moet komen
+
+
+// wacht totdat het geklikt wordt
+navButton.addEventListener.toggle
+
+// voeg een class toe aan de nav
---
+++ workshop_user-interaction/ui.js
@@ -0,0 +1,30 @@
+
+// let showMoreButton = document.querySelector('.show-more');
+
+// let doSomething = function() {
+//     alert('Het werkt!');
+// }
+
+// showMoreButton.addEventListener('click', doSomething);
+
+
+// showMoreButton.addEventListener('click', function() {
+//     alert('Het werkt!');
+// });
+
+
+
+
+let showMoreButton = document.querySelector('.show-more');
+showMoreButton.addEventListener('click', function() {
+    showMoreButton.classList.add('showing-more');
+});
+
+
+// CONSOLE
+// document.querySelector('.radioMenu')
+
+// $0.classList.add('nav-active')
+// $0.classList.remove('nav-active')
+// OF
+// $0.classList.toggle('nav-active')
