cb7edbcfc7da8e86667b04fb49fe4bc600dd9c9f
vsheo <vireshsheoratan@gmail.com>
2024-12-05 21:33:13 +0100

    open en sluit radio nav #4
    er is nu JS voor het open maken en sluiten van de radio nav

--- scripts/openNav.js
+++ scripts/openNav.js
@@ -1,14 +1,25 @@
-// deze script selecteerd een button die de nav open maaktr.
+// deze script selecteerd een button die de nav open maakt.
 // de javascript heeft een eventlistener op de button.
 // Als het geklikt wordt dan wordt er een class toegevoed of weggehaald(toggle) aan de button.
 
 
-// selecteer de button
-let navButton = document.querySelector('.radio_menu')
-// selecteer de nav die op beeld moet komen
+// selecteer de button die de nav open maakt
+const navButton = document.querySelector('.open_nav')
 
+// selecteer de navigatie menu die op beeld moet komen
+const radioMenu = document.querySelector('.radio_menu')
 
 // wacht totdat het geklikt wordt
-navButton.addEventListener.toggle
+navButton.addEventListener('click', () => {
+    // voeg een class toe aan de nav, of haal het ervan af
+    radioMenu.classList.toggle('nav-active');
 
-// voeg een class toe aan de nav
+    // draai '<' om als de nav open is
+    if (radioMenu.classList.contains('nav-active')) {
+        navButton.textContent = '>';
+    }
+    // draai '>' om als het gesloten is
+    else {
+        navButton.textContent = '<';
+    }
+});
