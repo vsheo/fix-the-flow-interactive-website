4dc74e8fb6cdbd3ddf69b7fd2144b478ca602ffe
vsheo <vireshsheoratan@gmail.com>
2024-12-10 11:17:23 +0100

    workshop view transition

--- scripts/sectionArticleColor.js
+++ scripts/sectionArticleColor.js
@@ -1,81 +1,26 @@
 // // op de desktop website worden classes gezet voor de kleur van de sections(radiozenders + programma kaartjes)
 
-// // selecteer alle sections
-// const sections = document.querySelectorAll('details section');
+// selecteer de sections en de articles
+const sections = document.querySelectorAll('details section');
+const articles = document.querySelectorAll('section article');
 
-// // selecteer alle articles, per radiozender
-// const veronicaAllArticles = document.querySelectorAll('details:nth-of-type(1) section article');
-// const slamAllArticles = document.querySelectorAll('details:nth-of-type(2) section article');
-// const honderdpAllArticles = document.querySelectorAll('details:nth-of-type(3) section article');
-// const sublimeAllArticles = document.querySelectorAll('details:nth-of-type(4) section article');
+// de classes die toegevoegd moeten worden
+const CLASSES = ["primary", "neutral"]
 
-// // zet de bovenstaande variables in een array, om later door heen te gaan met een loop
-// const allArticles = ["veronicaAllArticles", "slamAllArticles", "honderdpAllArticles", "sublimeAllArticles"];
+const primaryColor = ["veronica-primary", "slam-primary", "honderdp-primary", "sublime-primary"]
+const backgroundColor =["veronica-neutral"]
 
-// // de classes die toegevoegd moeten worden
-// const primary = ["primary"]
-// const background = ["neutral"]
-
-// const primaryColor = ["veronica-primary", "slam-primary", "honderdp-primary", "sublime-primary"]
-// const backgroundColor =["veronica-neutral", "slam-neutral", "honderdp-neutral", "sublime-neutral"]
-
-
-// // Dit alleen op desktop versie uit voeren
-// if (window.innerWidth >= 800) {
-//     for (let s = 0; s < primaryColor.length; s++) {
-//          // de classes primary en brand-primary aan de sections toevoegen
-//         sections[s].classList.add(primary[0]);
-//         sections[s].classList.add(primaryColor[s]);
-
-//         // de classes neutral en brand-neutral aan de articles toevoegen
-//         if (sections[s]) {
-//             for (let a = 0; a < allArticles[s].length; a++) {
-//                 allArticles[s][a].classList.add(background[0]);
-//                 allArticles[s][a].classList.add(backgroundColor[s]);
-//             }
-//         }
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-// // op de desktop website worden classes gezet voor de kleur van de sections(radiozenders + programma kaartjes)
-
-// // selecteer de sections en de articles
-// const sections = document.querySelectorAll('details section');
-// const articles = document.querySelectorAll('section article');
-
-// // de classes die toegevoegd moeten worden
-// const primary = ["primary"]
-// const background = ["neutral"]
-
-// const primaryColor = ["veronica-primary", "slam-primary", "honderdp-primary", "sublime-primary"]
-// const backgroundColor =["veronica-neutral"]
-
-// // Dit alleen op desktop versie uit voeren
-// if (window.innerWidth >= 800) {
-//     for (let s = 0; s < primaryColor.length; s++) {
-//          // de classes primary en brand-primary aan de sections toevoegen
-//         sections[s].classList.add(primary[0]);
-//         sections[s].classList.add(primaryColor[s]);
+// Dit alleen op desktop versie uit voeren
+if (window.innerWidth >= 800) {
+    for (let s = 0; s < primaryColor.length; s++) {
+         // de classes primary en brand-primary aan de sections toevoegen
+        sections[s].classList.add(CLASSES[0]);
+        sections[s].classList.add(primaryColor[s]);
         
-//         // de classes neutral en brand-neutral aan de articles toevoegen
-//         for (let a = 0; a < articles.length; a++) {
-//             articles[a].classList.add(background[0]);
-//             articles[a].classList.add(backgroundColor[0]);
-//         }
-//     }
-// }
-
-
-
-
-
-
+        // de classes neutral en brand-neutral aan de articles toevoegen
+        for (let a = 0; a < articles.length; a++) {
+            articles[a].classList.add(CLASSES[1]);
+            articles[a].classList.add(backgroundColor[0]);
+        }
+    }
+}
---
+++ scripts/sectionSnap.js
@@ -0,0 +1,41 @@
+// https://codepen.io/shooft/pen/WNVZYeZ
+
+const snapElement = document.querySelector(".snapElement");
+const snapDetails = document.querySelectorAll("details");
+console.log(snapElement, snapDetails);
+
+
+// run openSnapElement wanneer de pagina inlaad en wanneer de pagina verandert van grootte
+window.addEventListener('load', openSnapElement);
+window.addEventListener('resize', openSnapElement);
+
+snapDetails.forEach(section => {
+	section.addEventListener("click", scrollToTop);
+});
+
+
+// wanneer de pagina inlaad is de details element met de class snapElement open
+function openSnapElement() {
+    if (window.innerWidth >= 800) {
+        snapElement.open = true;
+    }
+}
+
+
+function scrollToTop(e) {
+	// alleen op desktop
+    if (window.innerWidth >= 800) {
+		// oude niet meer open
+		const nuOpen = document.querySelector(".snapElement");
+		if (nuOpen) {
+			// section die open is, dicht maken
+			nuOpen.open = false;
+			nuOpen.classList.remove("snapElement");
+		}
+		
+		// nieuwe naar het midden
+		const theDetail = e.currentTarget;
+		theDetail.scrollIntoView({ behavior: "smooth", block: "start" });
+		theDetail.classList.add("snapElement");
+	}
+}
