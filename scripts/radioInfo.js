c5751b5f4e1e3f5f2b4addda580c387332f9544d
vsheo <vireshsheoratan@gmail.com>
2024-12-19 00:22:28 +0100

    info pop up #15

---
+++ scripts/radioInfo.js
@@ -0,0 +1,24 @@
+// deze script brengt de info van de website op beeld
+
+// selecteer de button om de pop up te openen
+const meerInfo = document.querySelector('.information');
+
+// selecteer de pop up die te voorschijn komt
+const popUp = document.querySelector('.de_info');
+
+// sluit pop up button
+const openDicht = document.querySelector('.openClose');
+
+
+// lister naar een klikt op de button
+meerInfo.addEventListener('click', () => {
+    // haal de display none class weg van de pop up
+    popUp.classList.toggle('popUp_hidden');
+
+    // verander de text in de button, om aan te aangeefen dat het gesloten kan worden
+    if (!popUp.classList.contains('popUp_hidden')) {
+        openDicht.textContent = "Sluit de pop up";
+    } else {
+        openDicht.textContent = "Meer over ons";
+    }
+});
